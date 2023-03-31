bfe0f0585a470409d605efb5344449c7e91940a8
VladDrag <vdragomir@protonmail.com>
2023-03-31 08:35:29 +0200

    Add information and functionality to app via Dapper

--- HttpTrigger.cs
+++ HttpTrigger.cs
@@ -15,38 +15,61 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Aibel.Func
 {
-	public class MyClass
-	{
-		public string table_name { get; set; }
-	}
+	// public class MyClass
+	// {
+	// 	public string table_name { get; set; }
+	// }
 
-	public class GetDbData
-	{
-		public List<T> GetTableNamesAsync<T>(NpgsqlConnection con) where T : MyClass, new()
-		{
-			var tableNames = con.Query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'").ToList();
-			List<T> list = tableNames.Select(item => new T { table_name = item.table_name }).AsEnumerable().ToList();
-			return list;
-		}
-	}
+	// public class GetDbData
+	// {
+	// 	public List<T> GetTableNamesAsync<T>(NpgsqlConnection con) where T : MyClass, new()
+	// 	{
+	// 		var tableNames = con.Query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'").ToList();
+	// 		List<T> list = tableNames.Select(item => new T { table_name = item.table_name }).AsEnumerable().ToList();
+	// 		return list;
+	// 	}
+	// }
 
     public static class HttpTrigger
     {
 
         [FunctionName("HttpTrigger")]
-        public static Task<IActionResult> Run(
+        public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
 			string connectionString = System.Environment.GetEnvironmentVariable("connString");
 			using (var con = new NpgsqlConnection(connectionString)) 
 			{
-				con.Open(); 
-				var dbProvider = new GetDbData();
-				List<MyClass> tables = dbProvider.GetTableNamesAsync<MyClass>(con);
+				con.Open();
+				// create new table
+				//con.Execute("CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, name VARCHAR(40) NOT NULL, date TIMESTAMP NOT NULL)");
+
+
+				// delete table
+				//con.Execute("DROP TABLE IF EXISTS test");
+
+				// insert data *ONLY IF WE KNOW A TABLE EXISTS! -> we need to add extra code to check if table exists
+				//con.Execute("INSERT INTO test (name, date) VALUES(@Name, @Date)", new { Name = "Tim", Date = DateTime.Now });
+
+				// get data with Dapper
+				//var test = con.Query("SELECT * FROM test").ToList();
+	
+				// possible redundant code
+				//var dbProvider = new GetDbData();
+				//List<MyClass> tables = dbProvider.GetTableNamesAsync<MyClass>(con);
+
+				// in order to query async we need to use Dapper; 
+				// for that, we need to use QueryAsync instead of Query; 
+				// we cannot use ToList() with QueryAsync, so we need insert the items in a list after the operation finishes;
+				var tableInfo = await con.QueryAsync("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'");
+				var tables = tableInfo.ToList();
+
+				//serialization done with Newtonsoft.Json
+				var tableInfoJson = JsonConvert.SerializeObject(tables);
 
 				int tableCount = tables.Count();
-				return Task.FromResult<IActionResult>(new OkObjectResult("Number of tables is: " + tableCount));
+				return new OkObjectResult("Number of tables is: " + tableCount + " and the table names are: " + tableInfoJson);
 			}
         }
     }
